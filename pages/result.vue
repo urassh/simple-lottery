cb8d92b7f24172c777de4a3943509be6d107646f
Taichi Eto <38776373+TaichiEto@users.noreply.github.com>
2024-09-01 22:03:59 +0900

    Changes to be committed: 	modified:   pages/result.vue

--- pages/result.vue
+++ pages/result.vue
@@ -18,8 +18,8 @@ onMounted(() => {
 <template>
   <div class="min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center">
     <div class="w-full max-w-md p-4 bg-transparentWhite rounded-lg text-center">
-      <h2 class="text-xl font-bold mb-4">抽選結果</h2>
-      <p class="text-4xl mb-8">{{ resultNumber }}</p>
+      <h2 class="text-3xl font-bold mb-1">抽選結果</h2>
+      <p class="text-[200px] mb-8">{{ resultNumber }}</p>
       <NuxtLink to="/" class="bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded">
         戻る
       </NuxtLink>
