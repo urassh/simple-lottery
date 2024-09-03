2dcce7739a429da81f5c74fe5d1acbbc6a5d1dc7
urassh <urassh.1011@gmail.com>
2024-09-03 21:31:32 +0900

    feat: 禁止番号を追加

--- composables/numbers.ts
+++ composables/numbers.ts
@@ -1,11 +1,13 @@
 const maxNumber = 300;
+const excludedNumbers = ['000'];
 
 export const numberProvider = (numbers: string[]): string => {
     while (true) {
         const randomNumber = Math.floor(Math.random() * (maxNumber + 1));
         const formattedNumber = numberFormatter(randomNumber);
-        const isExist = numbers.includes(formattedNumber);
-        if (!isExist) {
+        const isValid = numberValidator(numbers, formattedNumber);
+
+        if (isValid) {
             return formattedNumber;
         }
     }
@@ -13,4 +15,11 @@ export const numberProvider = (numbers: string[]): string => {
 
 export const numberFormatter = (number: number) => {
     return number.toString().padStart(3, '0');
+}
+
+const numberValidator = (numbers: string[], formattedNumber: string) => {
+    const isExist = numbers.includes(formattedNumber);
+    const isExcludedNumber = excludedNumbers.includes(formattedNumber);
+    const invalid = isExist || isExcludedNumber;
+    return !invalid;
 }
