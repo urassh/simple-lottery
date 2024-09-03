7acedef2910777935d27e95668c119f877a8e749
urassh <urassh.1011@gmail.com>
2024-09-03 22:21:33 +0900

    設定: Jest、TypeScript、依存関係の更新

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,23 @@
-module.exports = {
+export default {
     moduleNameMapper: {
-      '^@/(.*)$': '<rootDir>/$1',
-      '^~/(.*)$': '<rootDir>/$1',
-      '^vue$': 'vue/dist/vue.common.js'
+        '^@/(.*)$': '<rootDir>/$1',
+        '^~/(.*)$': '<rootDir>/$1'
     },
-    moduleFileExtensions: ['js', 'vue', 'json'],
+    moduleFileExtensions: ['js', 'json', 'vue', 'ts'],
     transform: {
-      '^.+\\.js$': 'babel-jest',
-      '.*\\.(vue)$': '@vue/vue3-jest'
-    },
+        '^.+\\.js$': 'babel-jest',
+        '^.+\\.vue$': '@vue/vue3-jest',
+        '^.+\\.ts$': 'ts-jest'
+    },    
     collectCoverage: true,
     collectCoverageFrom: [
       '<rootDir>/components/**/*.vue',
       '<rootDir>/pages/**/*.vue'
-    ]
-  }
+    ],
+    testMatch: [
+        '<rootDir>/test/*.test.js',
+        '<rootDir>/test/*.spec.js',
+        '<rootDir>/test/*.test.ts',
+        '<rootDir>/test/*.spec.ts'
+    ],
+};
